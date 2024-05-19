d1603b24502596b16fdb120056dfcfbe169d56fb
pxmpkeen <pxmpkeen@gmail.com>
2024-05-19 19:46:43 +0400

    all

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,6 +2,6 @@
 
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err) {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
   if (err) throw err;
 });
--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,22 @@
 #!/usr/bin/node
 
-const request = require('request');
+// eslint-disable-next-line import/no-extraneous-dependencies
+import request from 'request';
 
 request(process.argv[2], (err, response, body) => {
   if (err) throw err;
+  // eslint-disable-next-line no-param-reassign
   body = JSON.parse(body);
 
   let result = body.reduce((x, y) => {
+    // eslint-disable-next-line no-param-reassign
     if (!x[y.userId]) x[y.userId] = 0;
+    // eslint-disable-next-line no-param-reassign
     if (y.completed) x[y.userId] += 1;
     return x;
   }, {});
 
-  result = Object.fromEntries(Object.entries(result).filter(([key, value]) => value > 0));
+  result = Object.fromEntries(Object.entries(result).filter(([, value]) => value > 0));
 
   console.log(result);
 });
